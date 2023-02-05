40accffa310f9f41d146c44aec2897075923b402
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-05 09:43:42 +0200

    Style: set up redux theme switcher

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { useMemo } from "react";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <>
-      <header>Header</header>
-      <main>Main</main>
-    </>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
   );
 }
 
