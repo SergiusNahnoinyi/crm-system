ec67aa378c115679379915021453a6e92b378a49
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-05 09:53:45 +0200

    Style/style-settings (#13)
    * Style: set up mui theme settings
    
    * Style: add fonts and global styles
    
    * Fix: fix a lint-staged command
    
    * Refactor: delete an unused file
    
    * Fix: delete an unexcisted import
    
    * Fix: delete space
    
    * Fix: move back space
    
    * Style: set up redux theme switcher

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
+import { useMemo } from "react";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <>
-      <header className="header">Header</header>
-      <main className="main">Main</main>
-    </>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
   );
 }
 
