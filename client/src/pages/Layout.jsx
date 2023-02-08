1ca7a8ed22a3fb6e20613953fdec826e4c9f94f7
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-08 14:13:59 +0200

    Feature: create a sidebar

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Box, useMediaQuery } from "@mui/material";
 
+import SideBar from "../components/SideBar";
 import NavBar from "../components/NavBar";
 
 export default function Layout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const isNonMobile = useMediaQuery("(min-width: 600px)");
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <SideBar
+        drawerWidth="250px"
+        isNonMobile={isNonMobile}
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box flexGrow={1}>
-        <NavBar />
+        <NavBar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
