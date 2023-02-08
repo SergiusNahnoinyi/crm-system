3714b0ead029c35eaa39ec091bab16a946cf502d
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2023-02-08 11:25:09 +0200

    Feature: create a navbar (#14)

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
 import { useMemo } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 
 import { useSelector } from "react-redux";
+
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+
 import { themeSettings } from "./theme";
 
-function App() {
+export default function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
-    <div>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
       </ThemeProvider>
-    </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
